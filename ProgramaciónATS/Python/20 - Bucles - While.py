293b5ca67718a26036df3f37eee7ced5acc95897
KarasuRoot <karasu.root@gmail.com>
2025-02-27 10:05:41 -0300

    update 27/02/2025

---
+++ ProgramaciónATS/Python/20 - Bucles - While.py
@@ -0,0 +1,21 @@
+#Bucle While (Mientras) - Raiz cuadrada - se ejecuta N veces o 0 veces
+'''
+import math
+numer = int(input('Digite un numero: '))
+while numer < 0: #Nos aseguramos que no sea negativo
+    print ('Error, debe ser un numero positivo')
+    numer = int(input('Digite un numero: '))
+print(f'\nSu raiz cuadrada de ' {number} ' es: {(math.sqrt(numer)):.2f}') '''
+
+
+'''Ejemplo 1 
+i = 0
+while i < 10:
+    print   ('Hola Mundo')
+    i += 1
+'''
+#Ejemplo 2
+i = 0
+while i < 10:
+    print(i)
+    i +=1
