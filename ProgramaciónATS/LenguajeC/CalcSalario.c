bd67cb17dfa3a14d519a469eb9a189ec9fe8df36
KarasuRoot <karasu.root@gmail.com>
2025-09-22 16:15:21 -0300

    UPDATE 22/09/2025

--- ProgramaciónATS/LenguajeC/CalcSalario.c
+++ ProgramaciónATS/LenguajeC/CalcSalario.c
@@ -10,5 +10,12 @@ int main(){
     scanf("%f",&valxhor);
     salario = (hor * valxhor);
     printf("\nSu salario es de: $%.2f",salario);
+    printf("\nPrecione Enter para salir...");
+    getchar();
+    getchar(); /* Se usa x2 getchar() -> Cuando ingresas el valor de las horas o del salario, escribes un número y luego presionas la tecla Enter. La función scanf() lee el número, pero deja el carácter de nueva línea (\n) que produce la tecla Enter en el búfer de entrada.
+
+Cuando pones solo un getchar() después de printf("\n\nPresione cualquier tecla para salir...");, ese getchar() lee inmediatamente ese carácter de \n que quedó pendiente. Como el getchar() ya "leyó" algo, el programa asume que la entrada se ha completado y continúa, lo que provoca que se cierre la ventana de la consola.
+
+El segundo getchar() es el que realmente espera a que presiones una tecla para continuar y, por lo tanto, mantiene la ventana abierta hasta que lo haces.*/
     return 0;
 }
